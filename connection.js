4e81e8c309cd30370176756f64d64d336297cf61
ysapphire <ysapphireo@gmail.com>
2019-02-18 09:26:54 +0200

    Simple connection and insertOne MongoDB

--- connection.js
+++ connection.js
@@ -1,6 +1,24 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/Books', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/Books', { useNewUrlParser: true }, (err, client) => {
     if (err) throw err;
     console.log('Connected to db');
-})
+
+
+    var db = client.db('Books');
+    db.collection('Books').insertOne({
+        title: 'The Devil in the White city',
+        author: 'Erik Larson',
+        genre: 'History',
+        published: 2003
+    }, (err, result) => {
+        if (err) {
+            console.log('Wrong ', err);            
+        }
+        console.log(result.ops);
+    
+    });
+
+    client.close();
+
+});

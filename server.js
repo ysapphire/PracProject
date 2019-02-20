926ffcba9ee4b60ad46a9c6f87baa71b7b2ca48d
ysapphire <ysapphireo@gmail.com>
2019-02-20 14:46:45 +0200

    Added POST/GET for users

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 //імпортую монгус і схему
 var {mongoose} = require('./models/mongoose');
 var {bookModel} = require('./models/bookSchema');
+var {userModel} = require('./models/userSchema');
 
 mongoose.Promise = global.Promise; // це ж для подальшого використання .then() ?
 
@@ -37,6 +38,27 @@ app.post('/list', (req, res) => {  //перевіряв як працювати 
 app.get('/list', (req, res) => {
     bookModel.find().then((books) => {
         res.send({books});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.post('/users', (req, res) => {  //перевіряв як працювати з Postman 
+    var user = new userModel({
+       name: req.body.name,
+       email: req.body.email,
+       password: req.body.password
+    });
+    user.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/users', (req, res) => {
+    userModel.find().then((users) => {
+        res.send({users});
     }, (err) => {
         res.status(400).send(err);
     });
